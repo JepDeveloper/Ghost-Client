66fbb7769cbec841e4ab7959fac2d586abe98a87
iCyphyDevvvv <72982537+PandaCyphy@users.noreply.github.com>
2021-08-01 16:59:44 +0530

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,1 @@
-if game.PlaceId == 286090429 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/PandaCyphy/Ghost-Client/main/Games/Arsenal.lua"))()
-end
-if game.PlaceId == 1962086868 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/PandaCyphy/Ghost-Client/main/Games/TowerOfHell.lua"))()
-end
+print"eaf"
